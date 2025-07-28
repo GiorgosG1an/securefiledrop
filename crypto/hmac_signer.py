e67d2d5e21def85c6cf02755e5f6ce2dd2148b9f
Georgios Giannopoulos <georgegiannopoulos2@gmail.com>
2025-07-28 14:18:35 +0300

    Implement HMAC logging functionality with key management

---
+++ crypto/hmac_signer.py
@@ -0,0 +1,57 @@
+import os
+import hmac
+import hashlib
+import base64
+from datetime import datetime
+import datetime
+
+LOG_KEY_PATH = "keys/logkey.bin"
+LOG_FILE = "logs/access.log"
+
+def load_log_key() -> bytes:
+    """
+    Loads a cryptographic key from the specified log key path.
+
+    Returns:
+        bytes: The loaded or newly generated cryptographic key.
+    """
+
+    if not os.path.exists(LOG_KEY_PATH):
+        key = os.urandom(32)
+        os.makedirs(os.path.dirname(LOG_KEY_PATH), exist_ok=True)
+
+        with open(LOG_KEY_PATH, 'wb') as f:
+            f.write(key)
+
+    else:
+        with open(LOG_KEY_PATH, 'rb') as f:
+            key = f.read()
+
+    return key
+
+def log_event(action: str, filename:str) -> None:
+    """
+    Logs an event with an HMAC signature for integrity.
+
+    Args:
+        action (str): The action performed (e.g., "upload", "download").
+        filename (str): The name of the file involved in the event.
+
+    Returns:
+        None
+
+    The log entry includes a UTC timestamp, the action, the filename, and a base64-encoded HMAC-SHA256 signature.
+    """
+    key = load_log_key()
+    timestamp = datetime.datetime.now(datetime.timezone.utc)
+    message = f"{timestamp} {action} {filename}"
+
+    signature = hmac.new(key, message.encode(), hashlib.sha256).digest()
+    signature_b64 = base64.b64encode(signature).decode()
+
+    log_line = f"{message} {signature_b64} \n"
+
+    os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
+
+    with open(LOG_FILE, "a") as log:
+        log.write(log_line)
